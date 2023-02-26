71f5c9e46b62afd20a2414a7e4970ce3b3b954c2
Shalini1908 <singhparmar1908@gmail.com>
2023-02-26 23:22:00 +0530

    added deploy

---
+++ middleware/Authentication.js
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+const jwt = require("jsonwebtoken")
+
+
+const authenticate = (req,res,next)=>{
+ const navigate = useNavigate()
+   const token = req.headers.authorization
+
+   if(token){
+
+       const decoded = jwt.verify(token,"masai")
+
+       if(decoded){
+      const id =  decoded.userID
+      req.body.userID = id
+        next()
+
+       }else{
+      res.send("please login first")
+      navigate("/login")
+       }
+   }else{
+
+       res.send("please login first")
+       navigate("/login")
+   }
+
+}
+
+module.exports = { authenticate } 
